ccdb0ec8f4ec2e67e0a73f32997aac9c2e0bcc0c
user <user@192.168.34.181>
2026-09-24 10:08:23 +0200

    Planet beearbeitet

--- StarSim2099/src/de/oszimt/starsim2099/Pilot.java
+++ StarSim2099/src/de/oszimt/starsim2099/Pilot.java
@@ -9,6 +9,34 @@ package de.oszimt.starsim2099;
 public class Pilot {
 
 	// Attribute
+	private double posX;
+	private double posY;
+	private String grad;
+	private String name;
+	public double getPosX() {
+		return posX;
+	}
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+	public double getPosY() {
+		return posY;
+	}
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+	public String getGrad() {
+		return grad;
+	}
+	public void setGrad(String grad) {
+		this.grad = grad;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	// Methoden
 
--- StarSim2099/src/de/oszimt/starsim2099/Planet.java
+++ StarSim2099/src/de/oszimt/starsim2099/Planet.java
@@ -9,8 +9,43 @@ package de.oszimt.starsim2099;
 public class Planet {
 
 	// Attribute
-	
+	double posX;
+	double posY;
+	int anzahlHafen;
+	String name;
 	// Methoden
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public int getAnzahlHafen() {
+		return anzahlHafen;
+	}
+
+	public void setAnzahlHafen(int anzahlHafen) {
+		this.anzahlHafen = anzahlHafen;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	// Darstellung
 	public static char[][] getDarstellung() {
