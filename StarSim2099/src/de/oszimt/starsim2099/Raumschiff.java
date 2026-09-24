78965929a207d8a63eff41a47f37479eaa82a6bd
user <user@192.168.34.181>
2026-09-24 09:22:58 +0200

    RAumschiff bearbeitet

--- StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
+++ StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
@@ -9,8 +9,62 @@ package de.oszimt.starsim2099;
 public class Raumschiff {
 
 	// Attribute
+	private double posX;
+	private double posY;
+	private int maxKapazitaet;
+	private String typ;
+	private String antrieb;
+	private int winkel;
 	
 	// Methoden
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public int getMaxKapazitaet() {
+		return maxKapazitaet;
+	}
+
+	public void setMaxKapazitaet(int maxKapazitaet) {
+		this.maxKapazitaet = maxKapazitaet;
+	}
+
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public String getAntrieb() {
+		return antrieb;
+	}
+
+	public void setAntrieb(String antrieb) {
+		this.antrieb = antrieb;
+	}
+
+	public int getWinkel() {
+		return winkel;
+	}
+
+	public void setWinkel(int winkel) {
+		this.winkel = winkel;
+	}
 
 	// Darstellung
 	public static char[][] getDarstellung() {
